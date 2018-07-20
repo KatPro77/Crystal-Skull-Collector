8e63bccf0d0a838fe632bf4aa62f5be9e3bb784d
Katerina Stanley <katerinastanley77@gmail.com>
2018-07-20 19:40:07 -0400

    finish layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+// 1) $(document).ready(function)
+
+// 2) create variables & set to 0 for:
+//     score/user total
+//     wins
+//     loss
+//     random number
+//     4x crystals??? (sub id instead?)
+
+// 3) display wins/losses and total score
+//         - CREATE FUNCTION / use IDs for HTML  - (get ElementByID????)
+
+// 4) display random number
+//         - CREATE FUNCTION 
+
+// 5) CREATE FUNCTION for reset the game
+//         function reset()
+
+// 6) assign numbers to each crystals (1-12)
+
+// 7) create IF and ELSE statements:
+
+//         if Wins (total score) = random number
+//             (create  winning alert)
+
+//         if Loss > random number
+//             (create decorative loss alert) - GAME OVER
+
+//       use RESET funvtion
+
+// REPEAT THIS FO ALL THE GEMS!! / or use # for all of them?? - check the hangman game
+
+// 8) create on key/click event 
+
+// -->
